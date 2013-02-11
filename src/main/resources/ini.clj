cae99e6884b47e864ffe2bcafec3175622fcefe2
Armando Blancas <abm221617@gmail.com>
2013-02-11 08:56:44 -0800

    Fixed multiple values in ini sample.

--- src/main/resources/ini.clj
+++ src/main/resources/ini.clj
@@ -15,13 +15,15 @@
   "Parses this rule:  pair := ID = field ( , field )* and returns the
    name-value pair in a vector. Operator <?> allows an error message to
    state that it expects a property, as oposed to saying it expects a letter."
-  (bind [f (<?> identifier "property") _ (sym \=) v (comma-sep1 (field ",\n"))]
+  (bind [f (<?> identifier "property")
+	 _ (sym \=) v
+	 (comma-sep1 (field ",\n"))]
     (return [f v])))
 
 (def group
   "Parses this rule:  group := [ field ] pair+"
   (bind [hdr (brackets (field "]\n")) rec (many1 pair)]
-    (return [hdr (apply hash-map (flatten rec))])))
+    (return [hdr (apply hash-map (reduce concat [] rec))])))
 
 (def ini
   "Parses:  init := group+"
