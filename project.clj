4def00c6308aac6a755fd9506b3d49bb7f45c94b
Armando Blancas <abm221617@gmail.com>
2013-03-17 17:15:20 -0700

    Bumped up the version.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.blancas/kern "0.5.0"
+(defproject org.blancas/kern "0.6.0"
   :description "A Parser Combinators Library"
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/kern"
   :plugins [[codox "0.6.4"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/main/clojure" "src/main/resources"]
   :codox {:sources ["src/main/clojure"] :output-dir "codox"}
   :jvm-opts ["-Dfile.encoding=UTF-8"])
