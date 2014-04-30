264843105739975c16f92d5b8072a27d7da4e0d2
Armando Blancas <abm221617@gmail.com>
2014-04-30 11:56:41 -0700

    Bumped up to version 1.0.0; updated dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.blancas/kern "0.7.0"
+(defproject org.blancas/kern "1.0.0"
   :description "A Parser Combinators Library"
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/kern"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :jvm-opts ["-Dfile.encoding=UTF-8"]
@@ -11,7 +11,14 @@
     {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
      :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
      :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+     :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
      :dev {:resource-paths ["src/main/resources" "src/test/resources"]
-           :dependencies [[midje "1.5.0" :exclusions [org.clojure/clojure]]]
+           :dependencies [[org.clojure/clojure "1.6.0"]
+                          [org.clojure/tools.macro "0.1.2"]
+                          [org.clojure/tools.trace "0.7.8"]
+                          [org.clojure/tools.nrepl "0.2.3"]
+                          [midje "1.6.3" :exclusions [org.clojure/clojure]]
+                          [criterium "0.4.3"]
+                          [jline "1.0"]]
            :plugins [[codox "0.6.4"]]
            :codox {:sources ["src/main/clojure"] :output-dir "codox"}}})
